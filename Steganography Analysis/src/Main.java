5ea6dfa98778c3e609aa673acc95c00bcf118c41
vm73@duke.edu <vm73@duke.edu>
2018-04-13 18:41:25 -0400

    LSB

--- Steganography Analysis/src/Main.java
+++ Steganography Analysis/src/Main.java
@@ -1,9 +1,13 @@
+import java.awt.image.BufferedImage;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Main Calling Code
+		DCT_Encrypt dct=new DCT_Encrypt();
+		int[] ar= {0,1,1,1,0};
+		BufferedImage img=new BufferedImage(width, height, imageType)
+		dct.encodeText("C:\\Users\\Vishnutej\\Desktop\\fedal.jpg", "hello", ar);
 	}
 
 }
